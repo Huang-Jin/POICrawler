1b7426ad3ef82c4accadb80131567bafc6005d64
Huang-Jin <1309740625@qq.com>
2020-03-17 17:37:26 +0800

    finished the part of mysql database.

--- POICrawler/main.cpp
+++ POICrawler/main.cpp
@@ -1,107 +1,116 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <sstream>
 //#include <iomanip>
 //#include <WinSock2.h>
 #include <string>
-#include <list>
+#include <vector>
 #include <memory>
 
 #include "Crawler.h"
 #include "mysqlHelper.h"
 
-#define Len 100
-#define PORT 443
+using namespace std;
 
-using std::cout;
-using std::endl;
-using std::string;
-
-struct stPOI
+struct POI
 {
-	char h_name[Len];
-	char h_prov[Len];
-	char h_city[Len];
-	char h_area[Len];
-	char h_adress[Len];
-	char h_type[Len];
-	char h_eastnorth[Len];
+	char name[MAXBYTE];
+	char prov[MAXBYTE];
+	char city[MAXBYTE];
+	char district[MAXBYTE];
+	char address[MAXBYTE];
+	char type[MAXBYTE];
+	char latlon[MAXBYTE];
 };
 
-struct stHtml
-{
-	char h_address[Len];
-};
-
-typedef std::list<struct stHtml> stHtmlList;
-stHtmlList list_Html;
-
-const string strResHead = "/poi/amap/district/420102/";
+vector<string> g_htmls;
+const string strUrlHead = "/poi/amap/district/420102/";
+const string g_table_name = "JiangAnDistrict";
 string g_strHost = "";
-string g_strResource = "";
-std::stringstream strStream;
-string strHtml = "";
-string g_place[Len] = { "" };
-std::ofstream real_file("POI_real.txt", std::ios::app);
-int g_iBegin = 0;
-int g_iHtml = 0;
+int g_index = 0;
+int g_page = 0;
+
+MySQLParam param;
+MySQLHelper sqlHelper(&param);
 
 HttpsClient g_client;
 
+void GetLastIndex(int & page, int & index) {
+	string sql = "select pageNum, pageIndex from " + g_table_name + " order by id desc limit 1;";
+	MYSQL_RES * res = sqlHelper.query(sql.c_str());
+	MYSQL_ROW row;
+	if (res != NULL && (row = mysql_fetch_row(res)) != NULL) {
+		page = atoi(row[0]);
+		index = atoi(row[1]);
+	}
+	else {
+		page = 0;
+		index = 0;
+	}
+}
+
+bool Write2MySQL(POI & poi) {
+	string sql = "";
+	sql += "insert into " + g_table_name + " values(id, '" + string(poi.name)
+		+ "', '" + string(poi.prov)
+		+ "', '" + string(poi.city)
+		+ "', '" + string(poi.district)
+		+ "', '" + string(poi.address)
+		+ "', '" + string(poi.type)
+		+ "', '" + string(poi.latlon)
+		+ "', '" + to_string(g_page)
+		+ "', '" + to_string(g_index + 1)
+		+ "', NULL);";
+
+	sqlHelper.query(sql.c_str());
+	return true;
+}
+
 bool GetPoiData(string & strResource)
 {
-	cout << "Try to connect with " << strResource << "\r\n";
+	cout << "Connect with " << strResource << "\r\n";
 	shared_ptr<char> html(g_client.get(g_strHost, strResource), free);
 	if (html.get() == NULL) return false;
 
-	stPOI stPoi;
+	POI stPoi;
 	const char *pBuf = NULL;
 
 	pBuf = strstr(html.get(), "<title>");
 	if (pBuf != NULL)
 	{
-		sscanf_s(pBuf, "%*[^>]>%[^-]", stPoi.h_name, Len);
+		sscanf_s(pBuf, "%*[^>]>%[^-]", stPoi.name, MAXBYTE);
 		pBuf = strstr(pBuf, "text-muted");
-		sscanf_s(pBuf, "%*[^>]>%*[^>]>%*[^>]>%[^<]", stPoi.h_prov, Len);
+		sscanf_s(pBuf, "%*[^>]>%*[^>]>%*[^>]>%[^<]", stPoi.prov, MAXBYTE);
 		pBuf += sizeof("text-muted");
 		pBuf = strstr(pBuf, "text-muted");
-		sscanf_s(pBuf, "%*[^>]>%*[^>]>%*[^>]>%[^<]", stPoi.h_city, Len);
+		sscanf_s(pBuf, "%*[^>]>%*[^>]>%*[^>]>%[^<]", stPoi.city, MAXBYTE);
 		pBuf += sizeof("text-muted");
 		pBuf = strstr(pBuf, "text-muted");
-		sscanf_s(pBuf, "%*[^>]>%*[^>]>%*[^>]>%[^<]", stPoi.h_area, Len);
+		sscanf_s(pBuf, "%*[^>]>%*[^>]>%*[^>]>%[^<]", stPoi.district, MAXBYTE);
 		pBuf += sizeof("text-muted");
 		pBuf = strstr(pBuf, "text-muted");
-		sscanf_s(pBuf, "%*[^>]>%*[^>]>%[^<]", stPoi.h_adress, Len);
+		sscanf_s(pBuf, "%*[^>]>%*[^>]>%[^<]", stPoi.address, MAXBYTE);
 		pBuf += sizeof("text-muted");
 		pBuf = strstr(pBuf, "text-muted");
 		pBuf += sizeof("text-muted");
 		pBuf = strstr(pBuf, "text-muted");
-		sscanf_s(pBuf, "%*[^>]>%*[^>]>%[^<]", stPoi.h_type, Len);
+		sscanf_s(pBuf, "%*[^>]>%*[^>]>%[^<]", stPoi.type, MAXBYTE);
 		pBuf += sizeof("text-muted");
 		pBuf = strstr(pBuf, "text-muted");
-		sscanf_s(pBuf, "%*[^>]>%*[^>]>%[^<]", stPoi.h_eastnorth, Len);
+		sscanf_s(pBuf, "%*[^>]>%*[^>]>%[^<]", stPoi.latlon, MAXBYTE);
 	}
 
-	real_file << stPoi.h_name << "\t"
-		<< stPoi.h_prov << "\t"
-		<< stPoi.h_city << "\t"
-		<< stPoi.h_area << "\t"
-		<< stPoi.h_type << "\t"
-		<< stPoi.h_eastnorth << endl;
-	cout << "Wrote the data.\r\n";
-
+	Write2MySQL(stPoi);
 	return true;
 }
 
 bool ParseUrl(const string & url, string & host, string & src)
 {
 	const char * pos_str = strstr(url.c_str(), "http://");
-	if (pos_str == NULL)
-	{
+	if (pos_str == NULL) {
 		pos_str = url.c_str();
 	}
-	else
-	{
+	else {
 		pos_str = pos_str + strlen("http://");
 	}
 
@@ -111,12 +120,10 @@ bool ParseUrl(const string & url, string & host, string & src)
 	sscanf_s(pos_str, "%[^/]%s", host_c, MAXBYTE, resource_c, MAXBYTE);
 
 	host = host_c;
-	if (strlen(resource_c) != 0)
-	{
+	if (strlen(resource_c) != 0) {
 		src = resource_c;
 	}
-	else
-	{
+	else {
 		src = "/";
 	}
 
@@ -125,116 +132,63 @@ bool ParseUrl(const string & url, string & host, string & src)
 
 bool ParseHtml()
 {
-	cout << "Try to connect with " << g_strResource << endl;
-
-	shared_ptr<char> html(g_client.get(g_strHost, g_strResource), free);
+	string url = strUrlHead + to_string(g_page) + ".html";
+	shared_ptr<char> html(g_client.get(g_strHost, url), free);
 	if (html.get() == NULL) return false;
 
+	char temp[MAXBYTE] = { 0 };
 	const char *buf = html.get();
-	while (buf)
-	{
+	while (buf) {
 		buf = strstr(buf, "<tr>");
-		if (buf == NULL)
-		{
+		if (buf == NULL) {
 			break;
 		}
 		buf = strstr(buf, "<td>");	// match "<td>"
-		if (buf == NULL)
-		{
+		if (buf == NULL) {
 			break;
 		}
 
-		stHtml html;
-		sscanf_s(buf, "%*[^\"]\"%[^\"]", html.h_address, Len);	// get the url between " ".
-		list_Html.push_back(html);
+		sscanf_s(buf, "%*[^\"]\"%[^\"]", temp, MAXBYTE);	// get the url between " ".
+		g_htmls.emplace_back(temp);
 	}
-
-	strStream.clear();
-	strStream << ++g_iHtml;
-	strStream >> strHtml;
-	g_strResource = strResHead + strHtml + ".html";
-
-	WSACleanup();
 	return true;
 }
 
 int main()
 {
-	MySQLParam param;
-	MySQLHelper sqlHelper(&param);
 	sqlHelper.connect();
 
-	char * sql = "create table test (name varchar(20), province varchar(20), city varchar(10), district varchar(15), address varchar(50), type varchar(30), LatLon varchar(100));";
-	sqlHelper.query(sql);
+	string sql = "create table " + g_table_name + " (id bigint(20) AUTO_INCREMENT primary key, name varchar(255), province varchar(20), "
+		+ "city varchar(10), district varchar(15), address varchar(255), type varchar(255), LatLon varchar(50), "
+		+ "pageNum int(4), pageIndex int(4), "
+		+ "update_time timestamp default current_timestamp on update current_timestamp"
+		+ ") character set utf8;";
+	sqlHelper.query(sql.c_str());
 
 	g_strHost = "www.poi86.com";
 
-	std::ifstream save_file("Save.txt");
-	save_file >> strHtml >> g_iBegin;
-	save_file.close();
-	g_iHtml = atoi(strHtml.c_str());
-
-	if (g_iHtml <= 0)
-	{
-		char cTem;
-		cout << "The starting point MAY be broken. First time for \"Y/y\", or check the starting point in \"Save.txt\"!\n";
-		std::cin >> cTem;
-		if (cTem == 'y' || cTem == 'Y')
-		{
-			g_iHtml = 1;
-			strHtml = "1";
-		}
-		else
-		{
-			std::cout << "Error for starting point.\r\n";
-			system("pause");
-			return -1;
-		}
+	GetLastIndex(g_page, g_index);
+	if (g_page <= 0) {
+		cout << "There are no data in log, using default configuration.\n";
+		g_page = 1;
+		g_index = 0;
+	}
+	else {
+		cout << "Start from page " << g_page << ", index " << g_index << ".\n";
 	}
 
-	if (g_iBegin < 0 || g_iBegin >= 50)
-	{
-		cout << "The starting point MUST be broken!\r\n";
-		system("pause");
-		return -1;
-	}
-
-	g_strResource = strResHead + strHtml + ".html";
-	
 	while (ParseHtml())
 	{
-		int iCount = 0;
-		cout << g_iHtml - 1 << "." << endl;
-		for (auto it = list_Html.begin(); it != list_Html.end(); it++)
+		cout << g_page << "." << endl;
+		for (; g_index < g_htmls.size(); g_index++)
 		{
-			++iCount;
-			if (g_iBegin)
-			{
-				g_iBegin--;
-				continue;
-			}
-
-			cout << iCount << ".\t";
-			string strBuf = it->h_address;
-			GetPoiData(strBuf);
-
-			if (iCount >= 44)
-			{
-				std::ofstream save_file("Save.txt");
-				save_file << g_iHtml << " " << 0;
-				save_file.close();
-			}
-			else
-			{
-				std::ofstream save_file("Save.txt");
-				save_file << g_iHtml - 1 << " " << iCount + 1;
-				save_file.close();
-			}
+			GetPoiData(g_htmls[g_index]);
 		}
-		list_Html.clear();
+		g_page++;
+		g_index = 0;
+		g_htmls.clear();
 	}
 
-	real_file.close();
 	system("pause");
 	return 0;
 }
--- POICrawler/mysqlHelper.h
+++ POICrawler/mysqlHelper.h
@@ -7,15 +7,15 @@ struct MySQLParam {
 	const char * _host = "127.0.0.1"; //本地MySQL服务器
 	const char * _user = "root"; //连接MySQL的用户名
 	const char * _passwd = "644152350+"; //MySQL服务器密码
-	const char * _db = "poi_data"; //要连接的数据库名
+	const char * _db = "POIData"; //要连接的数据库名
 	unsigned int _port = 3306; //MySQL服务器的端口，默认是3306
 	const char *unix_socket = NULL; //unix_socket是unix下的，在windows下就把它设置为NULL
-	unsigned long client_flag = 0; //一般为
+	unsigned long client_flag = 0; //一般为0;
 };
 
 class MySQLHelper {
 	MySQLParam * _pParam;
-	MYSQL _mysql, * _pSock;
+	MYSQL _mysql;
 	MYSQL_RES * _pResult; //保存结果集
 	MYSQL_ROW _row; //存储结果集中的一行
 
@@ -26,7 +26,7 @@ public:
 
 	~MySQLHelper() {
 		mysql_free_result(_pResult);
-		mysql_close(_pSock);
+		mysql_close(&_mysql);
 	}
 
 	bool connect();
@@ -35,20 +35,31 @@ public:
 
 bool MySQLHelper::connect() {
 	mysql_init(&_mysql);
-	_pSock = mysql_real_connect(&_mysql, _pParam->_host, _pParam->_user, _pParam->_passwd, _pParam->_db, _pParam->_port, _pParam->unix_socket, _pParam->client_flag);
-	if (_pSock == NULL) //连接MySQL失败
+	if (!mysql_real_connect(&_mysql, _pParam->_host, _pParam->_user, _pParam->_passwd, 
+		_pParam->_db, _pParam->_port, _pParam->unix_socket, _pParam->client_flag))
 	{
 		cout << mysql_error(&_mysql) << endl;
 		mysql_close(&_mysql);
 		return false;
 	}
 
+	if (!mysql_set_character_set(&_mysql, "utf8"))
+	{
+		printf("New client character set: %s\n",
+			mysql_character_set_name(&_mysql));
+	}
+	else return false;
+
 	return true;
 }
 
 MYSQL_RES * MySQLHelper::query(const char * sql) {
+	_pResult = NULL;
 	if (mysql_query(&_mysql, sql) == 0) {
 		_pResult = mysql_store_result(&_mysql);
+	}
+	else {
+		cout << mysql_error(&_mysql) << endl;
 	}
 	return _pResult;
 }
